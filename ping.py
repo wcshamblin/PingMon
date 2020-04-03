ba46282fe040465275a45037addbd8ffa982c355
wcshamblin <will@shamblin.org>
2020-04-03 18:44:25 -0400

    Initial commit

---
+++ ping.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import csv
+import os
+from datetime import datetime
+from time import sleep
+dformat = '%R'
+phost='google.com'
+pingcsv = open('ping.csv', 'w', newline='')
+writer = csv.writer(pingcsv, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+while True:
+    pcache=[]
+    cping=os.popen('ping -c 10 ' + phost).read().strip().split('\n')
+    for row in cping[1:-4]:
+        try:
+             pcache.append((float(row.split(' ')[-2].split('=')[1])))
+        except:
+             pcache.append(0.0)
+    pcache=[round(round(sum((pcache)), 2)/len(pcache), 2)]
+    writer.writerow([str(datetime.now().strftime(dformat)), float(pcache[0])])
+    sleep(60)
---
+++ pplot.py
@@ -0,0 +1,27 @@
+import numpy as np
+import plotly.graph_objects as go
+import pandas as pd
+pid=pd.read_csv('./ping.csv')
+
+trace1 = go.Scattergl(
+    x=pid['time'],
+    y=pid['ping'],
+    mode='markers',
+    marker=dict(
+        opacity=0.8
+    ),
+)
+layout = go.Layout(
+    title='Time vs. Ping',
+    xaxis=dict(
+        title='Time'
+    ),
+    yaxis=dict(
+        title='Ping (ms)'
+    ),
+    hovermode='closest',
+    #showlegend=True
+)
+
+figure = go.Figure(data=trace1, layout=layout)
+figure.show()
